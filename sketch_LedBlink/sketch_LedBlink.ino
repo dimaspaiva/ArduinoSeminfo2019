51778f608233432fb2b4552709b3203f595ec7d1
Dimas Paiva <dimasalpaiva@gmail.com>
2019-08-30 06:18:01 -0300

    Second Step

--- sketch_LedBlink/sketch_LedBlink.ino
+++ sketch_LedBlink/sketch_LedBlink.ino
@@ -2,7 +2,12 @@ void setup() {
   // put your setup code here, to run once:
  pinMode(2, OUTPUT);
  digitalWrite(2, 1);
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(2, 1);
+  delay(250);
+  digitalWrite(2, 0);
+  delay(250);
 }
