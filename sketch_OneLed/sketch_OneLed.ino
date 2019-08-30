43d6aa00e05187c7cd02d4aa7accb33a3c7fca19
Dimas Paiva <dimasalpaiva@gmail.com>
2019-08-30 06:12:35 -0300

    First Step

---
+++ sketch_OneLed/sketch_OneLed.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(2, OUTPUT);
+ pinMode(3, OUTPUT);
+ pinMode(4, OUTPUT);
+ digitalWrite(2, 0);
+ digitalWrite(3, 0);
+ digitalWrite(4, 0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
